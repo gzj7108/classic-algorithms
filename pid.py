8662acf28cb57e0f08c7cb8fb383197f52f42d10
gzj7108 <798159825@qq.com>
2020-01-22 20:24:22 +0800

    Update pid.py

--- pid.py
+++ pid.py
@@ -11,7 +11,7 @@ class Pid():
         self.kp=0.0
         self.ki=0.0
         self.kd=0.0
-        self.alteration_times=1000
+        self.iteration_times=1000
 
     def setparameters(self,kp,ki,kd):
         """set pid parameters"""
@@ -20,14 +20,14 @@ class Pid():
         self.kd=kd
         
     def tracksignal(self,ideal):
-        """function as track the ideal output by alterating the error"""
+        """function as tracking the ideal output by iterating the error"""
         ideal_output=ideal
         actual_output=0.0
         error=0.0
         error_sum=0.0
         error_difference=0.0
-
-        for i in range(self.alteration_times):
+        
+        for i in range(self.iteration_times):
             #pid dispersed expression
             error_difference=ideal_output-actual_output-error
             error=ideal_output-actual_output
